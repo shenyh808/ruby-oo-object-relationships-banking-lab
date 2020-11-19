4fa127ca3fffa711a2f90591327e2d1f5dc138e2
shen huang <shenyh33@gmail.com>
2020-11-19 00:08:54 -1000

    commit tree 4464ba463be1560bcefe6118f82b2badfb05eb12

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,32 @@
 class BankAccount
+    attr_accessor :bankaccount, :balance, :status 
+    attr_reader :name 
+
+    def initialize(bankaccount)
+        @bankaccount = bankaccount
+        @name = 'Avi'
+        @balance = 1000
+        @status = 'open'
+    end
+
+    def deposit(money)
+        self.balance = 2000
+    end
+
+    def display_balance
+        "Your balance is $#{balance}."
+    end
+
+    def valid?
+        if @status == "open" && balance > 0
+            true 
+        else
+            false 
+        end
+    end
+
+    def close_account 
+        @status = 'closed'
+    end
 
 end
